7b1f17f81d09917e1cb489e985296719e4db40c8
Robert <foersteroberthh@gmail.com>
2021-10-24 12:11:58 +0200

    partial pivot

--- main.py
+++ main.py
@@ -73,42 +73,73 @@ if eliminate:
         eingebauteLösungString.append(f"{ascii_l[idx]} = ")
         eingebauteLösungString.append(eingebauteLösung[idx])
     st.write("in Numpy eingebaute Lösung:", *eingebauteLösungString)
-    with st.expander("Details"):
-        s1, s2 = st.columns(2)
-        s1.write(np.array(a1))
-        s2.write(np.array(b1))
-        Xs, Ys = PolyGraph(eigeneLösung)
-        df = pd.DataFrame(dict(
-            x = Xs,
-            y = Ys
-        ))
-        myString = []
-        for idx, i in enumerate(eigeneLösung):
-            myString.append(f"{i}x\u00b3{len(eigeneLösung)-idx-1}")
-        fig = px.line(df, x="x", y="y", title="Graph: " + " + ".join(myString))
-        fig.update_xaxes(range=[-1, 6])
-        fig.update_yaxes(range=[-5, 5])
-        st.plotly_chart(fig)
+    superScriptDict = {
+        1: '',
+        2: '²',
+        3: '³',
+        4: '⁴',
+        5: '⁵',
+    }
+    Xs, Ys = PolyGraph(eigeneLösung)
+    df = pd.DataFrame(dict(
+        x = Xs,
+        y = Ys
+    ))
+    myString = []
+    for idx, i in enumerate(eigeneLösung):
+        if i != 0:
+            myString.append(f"{i}x{superScriptDict[len(eigeneLösung)-idx-1]}")
+    fig = px.line(df, x="x", y="y", title="Graph: " + " + ".join(myString))
+    fig.update_xaxes(range=[-1, 6])
+    fig.update_yaxes(range=[-5, 5])
+    st.plotly_chart(fig, use_container_width=True)
+    st.write("Eliminierungs Dreieck")
+    _ = [a1[i].append(b1[i]) for i in range(len(b1))]
+    st.write(np.array(a1))
+
+st.markdown("---")
 
 st.code(
     """
 def eliminieren(a, b):
-    for i in range(len(b)-1):
-        for j in range(i+1, len(b)):
-            if a[j][i] == 0:
-                continue
-            factor = a[i][i] / a[j][i]
-            for k in range(i, len(b)):
-                a[j][k] = a[i][k] - a[j][k]*factor
-            b[j] = b[i] - b[j]*factor
-        return a, b
+    for i in range(len(b)-1): # iteriere von der ersten bis zur vorletzten Spalte/"Koeffs von jeweils einer Variable"
+        if a[i][i] < 1.0e-12: # wenn sich Wert direkt über dem Dreieck 0 annähert probiere Reihenfolge zu ändern um 0-Division zu umgehen
+            for j in range(i+1, len(b)): # iteriere über Werte innerhalb der Spalte ab Spalte+1
+                if a[j][i] > a[i][i]: # wenn iterierter Wert größer als Wert über Dreieck (ca. Null)
+                    a[i], a[j] = a[j], a[i] # vertausche Reihen
+                    b[i], b[j] = b[j], b[i] # vertausche B/Y-Werte
+                    break # breche Schleife ab
+        for j in range(i+1, len(b)): # iteriere über Werte innerhalb der Spalte ab Spalte+1 für Dreiecksform insgesamt
+            if a[j][i] == 0: continue # überspringe wenn Wert schon 0
+            factor = a[i][i] / a[j][i] # Ermittle Verhältnis von aktuellem Koeff und dem Koeff über Dreieck
+            for k in range(i, len(b)): # iteriere über ausgewählte ganze Reihe
+                a[j][k] = a[i][k] - a[j][k]*factor # Setze Koeff auf 0 oder berechne anderen neuen Werte
+            b[j] = b[i] - b[j]*factor # Berechne neuen B/Y-Wert für Reihe
+    return a, b # Gebe LGS mit eliminiertem Dreieck zurück 
 
 def backsubstitution(a, b):
     x = [0 for _ in range(len(b))] # Jeder Variable wird erst 0 zugewiesen.
     x[len(b)-1] = b[len(b)-1] / a[len(b)-1][len(b)-1] # Variable unten rechts im LGS wird gelöst
     for i in range(len(b)-2, -1, -1): # iteriert von unten nach oben über restliche Gleichungen
-    sum_ax = sum([a[i][j] * x[j] for j in range(i+1, len(b))]) 
-    x[i] = (b[i] - sum_ax) / a[i][i]
-    return x
-"""
+        sum_ax = sum([a[i][j] * x[j] for j in range(i+1, len(b))]) # Addiere bisher gelöste Variablen * Koeff
+        x[i] = (b[i] - sum_ax) / a[i][i] # Berechne die neue Variable der Reihe
+    return x # Gebe gelöste Variablen zurück
+    """
 )
+
+st.markdown(
+            """
+            <div>
+                <a style='text-decoration: none; display:flex; align-items: center; justify-content: center; gap: 5px;' href="https://github.com/foersterrobert/MNIST-Drawer" target='_blank'>
+                    <h4>Der code ist auch auf GitHub</h4>
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+
+st.markdown("---")
+
+st.subheader("Videos über die Theorie")
+st.video("https://www.youtube.com/watch?v=ZDxONtacA_4")
+st.video("https://www.youtube.com/watch?v=i7f9PBe-j_Y")
